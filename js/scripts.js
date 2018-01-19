a7f3959e93fac4ed3e221c958b21284a3ac52307
Magdalena Szymczak <mwozniak.kontakt@gmail.com>
2018-01-19 18:38:54 +0100

    Add all files

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+	var carouselList = $("#carousel ul");
+	setInterval(changeSlide, 3000); 
+	function changeSlide(){
+		carouselList.animate({'marginLeft':-400}, 800, moveFirstSlide);
+	};
+	function moveFirstSlide(){
+		//carouselList.find("li:last").after(carouselList.find("li:first"));
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+
+	}
+	
+});
+
